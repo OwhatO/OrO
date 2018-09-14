0ad23e686e8ebf0f8d128952f92fa3e368ce1f30
Fenz <uukoo@163.com>
2018-09-14 23:32:21 +0800

    add view registerer for router

--- Router.js
+++ Router.js
@@ -1,8 +1,10 @@
+import View from 'https://oxo.fenzland.com/OvO/0.1/View.js';
 import { resolve, traceBack, dirname, } from 'https://oxo.fenzland.com/OsO/0.1/path.js';
 
 const BASE_PATH= Symbol( 'BASE_PATH', );
 const PAGE_DIR= Symbol( 'PAGE_DIR', );
 const ROUTES= Symbol( 'ROUTES', );
+const VIEW= Symbol( 'VIEW', );
 
 export default class Router
 {
@@ -13,5 +15,17 @@ export default class Router
 		this[BASE_PATH]= new URL( resolve( baseOn, basePath, ), ).pathname.replace( /\/$/, '', );
 		this[PAGE_DIR]= resolve( baseOn, pageDIr, );
 		this[ROUTES]= new Map;
+	}
+	
+	/**
+	 * Set a DOM as route container. and create a VIEW.
+	 * 
+	 * @param Element container
+	 * 
+	 * @return void
+	 */
+	showIn( container, )
+	{
+		this[VIEW]= new View( container, );
 	}
 }
