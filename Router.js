aeee08a3d7a42f16fb39853c602d972ff5ad354d
Fenz <uukoo@163.com>
2018-09-14 23:30:25 +0800

    add constructor of Router, construct with a base path and a base page directory

--- Router.js
+++ Router.js
@@ -1,5 +1,17 @@
+import { resolve, traceBack, dirname, } from 'https://oxo.fenzland.com/OsO/0.1/path.js';
+
+const BASE_PATH= Symbol( 'BASE_PATH', );
+const PAGE_DIR= Symbol( 'PAGE_DIR', );
+const ROUTES= Symbol( 'ROUTES', );
 
 export default class Router
 {
-	
+	constructor( basePath, pageDIr, )
+	{
+		const baseOn= dirname( traceBack(), );
+		
+		this[BASE_PATH]= new URL( resolve( baseOn, basePath, ), ).pathname.replace( /\/$/, '', );
+		this[PAGE_DIR]= resolve( baseOn, pageDIr, );
+		this[ROUTES]= new Map;
+	}
 }
